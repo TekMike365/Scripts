8aa8963b36956915b8abd47c4c69094d9f5bd6e8
TekMike365 <miskosklenka@gmail.com>
2024-02-13 16:18:17 +0100

    Shit broke, i fixed it. Also in the past 'Initial commit's a project-html script was added. *sigh*

--- project-html.sh
+++ project-html.sh
@@ -11,21 +11,21 @@ if [[ $# -gt 0 ]]; then
 	for arg in $@; do
 		if [[ $arg[1] == "-" ]]; then
 			if [[ $arg =~ "f" ]]; then
-				pflags=($pflags "f")
+				pflags=($pflags"f")
 				force=1
 			fi
 			if [[ $arg =~ "r" ]]; then
-				pflags=($pflags "r")
+				pflags=($pflags"r")
 				rm_readme=1
 			fi
 			if [[ $arg =~ "g" ]]; then
 				rm_git=1
 			fi
 			if [[ $arg =~ "t" ]]; then
-				cpflags=($cpflags "t")
+				cpflags=($cpflags"t")
 			fi
 			if [[ $arg =~ "b" ]]; then
-				cpflags=($cpflags "b")
+				cpflags=($cpflags"b")
 			fi
 		else
 			dir=$arg
@@ -38,7 +38,10 @@ scripts_dir=$0:A:h
 $scripts_dir/project.sh -g$pflags $dir
 
 echo "Adding files..."
+echo $cpflags
 cp $scripts_dir/res/html$cpflags/* $dir
+
+cd $dir
 
 if [[ rm_git -eq 0 ]]; then
 	echo "> Creating .gitignore"
