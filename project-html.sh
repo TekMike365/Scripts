c28554325b8e10d83c6a6f992542b1cf60aca9af
TekMike365 <miskosklenka@gmail.com>
2024-02-13 17:17:31 +0100

    added help to project-html

--- project-html.sh
+++ project-html.sh
@@ -1,22 +1,42 @@
 #!/bin/zsh
 
 dir="./new_project"
-rm_readme=0
-force=0
-rm_git=0
 cpflags=""
 pflags=""
+rm_git=0
 
 if [[ $# -gt 0 ]]; then
 	for arg in $@; do
 		if [[ $arg[1] == "-" ]]; then
+			if [[ $arg =~ "h" ]]; then
+				echo "
+Project-HTML. The project creation helper.
+Creates a html template (index.html, style.css, script.html),
+and initates a git repository in your directory of coise!
+
+Usage: project-html.sh [arguments](*) [path](**/*3)
+       
+Arguments:
+  -f	force, replaces the existing directory
+  -r	remove README, doesn't create readme
+  -g	git repository will not be initialized
+  -t	creates template (index.thml, style.css, script.html)
+	with tabs (spaces are default)
+  -b	creates just index.html
+
+*  You can string the arguments, ie. '-fr' is the same as '-f -r'.
+   Position doesn't matter
+** Path with a name of the folder, ie. /path/do/dir/project_name.
+*3 Underscores will be replaced by spaces in README file.
+"
+
+				return
+			fi
 			if [[ $arg =~ "f" ]]; then
 				pflags=($pflags"f")
-				force=1
 			fi
 			if [[ $arg =~ "r" ]]; then
 				pflags=($pflags"r")
-				rm_readme=1
 			fi
 			if [[ $arg =~ "g" ]]; then
 				rm_git=1
@@ -43,7 +63,7 @@ cp $scripts_dir/res/html$cpflags/* $dir
 
 cd $dir
 
-if [[ rm_git -eq 0 ]]; then
+if [[ $rm_git -eq 0 ]]; then
 	echo "> Creating .gitignore"
 	touch .gitignore
 
